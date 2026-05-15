31557b70d7a37cf8a81c02a5a4b33e7af92b3cc3
oamochi <146185786+oamochi@users.noreply.github.com>
2026-05-16 06:26:36 +0900

    doGetメソッドの処理を記述

--- src/main/java/sample_pkg01/HelloWorldServlet.java
+++ src/main/java/sample_pkg01/HelloWorldServlet.java
@@ -1,6 +1,7 @@
 package sample_pkg01;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -29,7 +30,16 @@ public class HelloWorldServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Hello World!</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hello World!</h1>");
+		out.println("<p>First Servlet</p>");
+		out.println("</body></html>");
+		//.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 }
